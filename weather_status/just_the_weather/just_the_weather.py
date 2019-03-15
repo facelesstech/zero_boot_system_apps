399456d411348f95a45d24d62bb48ea3fcc18980
facelessloser <facelessloser@facelessloser.thinkpad>
2019-03-15 15:58:25 +0000

    icon fix test4

--- weather_status/just_the_weather/just_the_weather.py
+++ weather_status/just_the_weather/just_the_weather.py
@@ -200,7 +200,7 @@ while True:
         else:
 #            iconFlag = True
 #            iconFlag = False
-            textSummary = myFontSmall.render(newCurrent, True, white) # Draw text
+            textSummary = myFontSmall.render(current, True, white) # Draw text
             screen.blit(textSummary,(10,80)) # Draw text
 
         iconLoad = pygame.image.load(icon_mapping(current.icon, 256))# Load up the photo you just took
